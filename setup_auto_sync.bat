5a5afe9d0380e418388fb0c43b6ee6c9d860ee75
goga7615 <igorkelt@gmail.com>
2025-08-16 18:44:46 +0300

    Initial setup

---
+++ setup_auto_sync.bat
@@ -0,0 +1,9 @@
+   @echo off
+   echo Настройка автоматической синхронизации...
+   echo.
+   git init
+   git add .
+   git commit -m "Initial setup"
+   echo.
+   echo Git репозиторий создан!
+   pause
